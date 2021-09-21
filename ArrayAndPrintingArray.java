67a5919b783e5bd90f938714d3879570cd644304
Raphael Viana <rnvdev.contato@gmail.com>
2021-09-21 14:03:48 -0300

    Create ArrayAndPrintingArray.java

---
+++ ArrayAndPrintingArray.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        String[] vect = new String[]{"Maria", "Bob", "Alex"};
+        for (String obj : vect) {
+            System.out.println(obj);
+        }
+        
+    }
+}
